bd214c5f7eb2b811dd3a06df250aad493619c908
Ari Artinian <git@artinian.ch>
2026-04-27 17:34:29 +0200

    feat: add PV-PQ Q-limit switching and Pg/Qg in result

--- PowerFlow.Runner/Program.cs
+++ PowerFlow.Runner/Program.cs
@@ -20,15 +20,19 @@ Console.WriteLine(
 );
 
 Console.WriteLine();
-Console.WriteLine($"{"Bus",-4} {"Vm (pu)",-10} {"Va (deg)",-10}");
+Console.WriteLine(
+    $"{"Bus", -4} {"Vm (pu)", -10} {"Va (deg)", -10} {"Pg (pu)", -10} {"Qg (pu)", -10}"
+);
 for (int i = 0; i < net.Buses.Count; i++)
-    Console.WriteLine($"{net.Buses[i].Id,-4} {result.Vm[i],-10:F4} {result.Va[i],-10:F3}");
+    Console.WriteLine(
+        $"{net.Buses[i].Id, -4} {result.Vm[i], -10:F4} {result.Va[i], -10:F3} {result.Pg[i], -10:F4} {result.Qg[i], -10:F4}"
+    );
 
 Console.WriteLine();
 Console.WriteLine(
-    $"{"From",-5} {"To",-5} {"P_ij (pu)",-12} {"Q_ij (pu)",-12} {"P_ji (pu)",-12} {"Q_ji (pu)",-12}"
+    $"{"From", -5} {"To", -5} {"P_ij (pu)", -12} {"Q_ij (pu)", -12} {"P_ji (pu)", -12} {"Q_ji (pu)", -12}"
 );
 foreach (var bf in result.BranchFlows)
     Console.WriteLine(
-        $"{bf.FromBusId,-5} {bf.ToBusId,-5} {bf.Pij,-12:F4} {bf.Qij,-12:F4} {bf.Pji,-12:F4} {bf.Qji,-12:F4}"
+        $"{bf.FromBusId, -5} {bf.ToBusId, -5} {bf.Pij, -12:F4} {bf.Qij, -12:F4} {bf.Pji, -12:F4} {bf.Qji, -12:F4}"
     );
